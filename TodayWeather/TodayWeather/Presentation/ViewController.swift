6c3de45d114d56db037bd1fec9756f9ead9aaf60
HyunRyeol Park <devpark435@gmail.com>
2024-05-13 20:41:21 +0900

    [Setting] #1 - Develop Branch 생성

--- TodayWeather/TodayWeather/Presentation/ViewController.swift
+++ TodayWeather/TodayWeather/Presentation/ViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 // ⚠️ 폴더 삭제 방지용 뷰컨트롤러 파일입니다.
-// 개인작업은 각 폴더 생성 후 진행해주세요.(Model, Network 동일)
+// 개인작업은 각 폴더 생성 후 진행해주세요.(Model, Network 동일!)
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
