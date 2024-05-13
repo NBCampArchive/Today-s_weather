9a8914a792f76f9b2e1444b4328189baa11c72f1
HyunRyeol Park <devpark435@gmail.com>
2024-05-13 20:18:09 +0900

    Create Project

---
+++ TodayWeather/TodayWeather/Presentation/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  TodayWeather
+//
+//  Created by 박현렬 on 5/13/24.
+//
+
+import UIKit
+// ⚠️ 폴더 삭제 방지용 뷰컨트롤러 파일입니다.
+// 개인작업은 각 폴더 생성 후 진행해주세요.(Model, Network 동일)
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+    }
+
+
+}
+
