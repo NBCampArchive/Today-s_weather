f798e2e4ea022192032fef691e9e66fbf7b03481
HyunRyeol Park <devpark435@gmail.com>
2024-05-14 14:12:51 +0900

    [Feat] #2 - Refactor camelCase

--- TodayWeather/TodayWeather/Model/CurrentResponseModel.swift
+++ TodayWeather/TodayWeather/Model/CurrentResponseModel.swift
@@ -38,14 +38,24 @@ struct Weather: Decodable {
 
 struct Main: Decodable {
     let temp: Double
-    let feels_like: Double
-    let temp_min: Double
-    let temp_max: Double
+    let feelsLike: Double
+    let tempMin: Double
+    let tempMax: Double
     let pressure: Int
     let humidity: Int
-    let sea_level: Int?
-    let grnd_level: Int?
+    let seaLevel: Int?
+    let grndLevel: Int?
+
+    enum CodingKeys: String, CodingKey {
+        case temp, pressure, humidity
+        case feelsLike = "feels_like"
+        case tempMin = "temp_min"
+        case tempMax = "temp_max"
+        case seaLevel = "sea_level"
+        case grndLevel = "grnd_level"
+    }
 }
+
 
 struct Wind: Decodable {
     let speed: Double
