86343109afcc956261f45ab8a559e9437a2f419f
HyunRyeol Park <devpark435@gmail.com>
2024-05-14 13:17:38 +0900

    [Feat] #2 - 위치정보 API Manager 추가

--- TodayWeather/TodayWeather/Resource/LocationManager.swift
+++ TodayWeather/TodayWeather/Resource/LocationManager.swift
@@ -5,4 +5,37 @@
 //  Created by 박현렬 on 5/14/24.
 //
 
-import Foundation
+import CoreLocation
+
+class LocationManager: NSObject, CLLocationManagerDelegate {
+    static let shared = LocationManager()
+    
+    private let locationManager = CLLocationManager()
+    private var completion: ((CLLocation?) -> Void)?
+    
+    private override init() {
+        super.init()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+    }
+    
+    func requestLocation(completion: @escaping (CLLocation?) -> Void) {
+        self.completion = completion
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.first else {
+            completion?(nil)
+            return
+        }
+        completion?(location)
+        locationManager.stopUpdatingLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Error: \(error.localizedDescription)")
+        completion?(nil)
+    }
+}
