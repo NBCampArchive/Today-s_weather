0c109fd21aeb060c5dcfe3488aed92cf5ad7ea08
HyunRyeol Park <devpark435@gmail.com>
2024-05-14 13:18:29 +0900

    [Feat] #2 - API Key Bundle 확장

--- TodayWeather/TodayWeather/Resource/TodayWeatherAPI++Bundle.swift
+++ TodayWeather/TodayWeather/Resource/TodayWeatherAPI++Bundle.swift
@@ -6,3 +6,38 @@
 //
 
 import Foundation
+
+// API 키를 Bundle에서 가져오기 위한 확장
+extension Bundle{
+    
+    // OpenWeatherMap API Key
+    var weatherApiKey: String{
+        guard let filePath = Bundle.main.path(forResource: "TodayWeatherAPI", ofType: "plist") else{
+            fatalError("Couldn't find file 'TodayWeatherAPI.plist'.")
+        }
+        
+        let plist = NSDictionary(contentsOfFile: filePath)
+        
+        guard let value = plist?.object(forKey: "Weather_API_KEY") as? String else{
+            fatalError("Couldn't find key 'API_KEY' in 'TodayWeatherAPI.plist'.")
+        }
+        
+        return value
+    }
+    
+    // Dust API Key
+    var dustApiKey: String{
+        guard let filePath = Bundle.main.path(forResource: "TodayWeatherAPI", ofType: "plist") else{
+            fatalError("Couldn't find file 'TodayWeatherAPI.plist'.")
+        }
+        
+        let plist = NSDictionary(contentsOfFile: filePath)
+        
+        guard let value = plist?.object(forKey: "Dust_API_KEY") as? String else{
+            fatalError("Couldn't find key 'API_KEY' in 'TodayWeatherAPI.plist'.")
+        }
+        
+        return value
+    }
+    
+}
