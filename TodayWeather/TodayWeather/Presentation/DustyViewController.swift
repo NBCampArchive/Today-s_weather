1687f98209804b36ff9e3594ac42011cf5c9bf31
HyunRyeol Park <devpark435@gmail.com>
2024-05-16 18:26:42 +0900

    [Feat] #4 - 커스텀 세그먼트 제작에 따른 제약조건 업데이트

--- TodayWeather/TodayWeather/Presentation/DustyViewController.swift
+++ TodayWeather/TodayWeather/Presentation/DustyViewController.swift
@@ -11,61 +11,35 @@ import Then
 
 class DustyViewController: UIViewController{
     
+    var backgroundColor: UIColor?
+    
     let dotAnimationView = DotAnimationView().then{
-//        $0.backgroundColor = UIColor(named: "dustFineColor")
-        $0.layer.cornerRadius = 130
+        $0.layer.cornerRadius = 140
         $0.layer.masksToBounds = true
-    }
-    
-    let segmentedControl = UISegmentedControl().then {
-        $0.insertSegment(withTitle: "Today", at: 0, animated: true)
-        $0.insertSegment(withTitle: "Detail", at: 1, animated: true)
-        $0.selectedSegmentIndex = 0
-        
-        $0.setBackgroundImage(UIImage(), for: .normal, barMetrics: .default)
-        $0.setBackgroundImage(UIImage(), for: .selected, barMetrics: .default)
-        $0.setDividerImage(UIImage(), forLeftSegmentState: .normal, rightSegmentState: .normal, barMetrics: .default)
-        $0.layer.borderWidth = 0
-        $0.layer.masksToBounds = true
-        
-        // Set text attributes
-        let normalAttributes: [NSAttributedString.Key: Any] = [
-            .foregroundColor: UIColor.white,
-            .font: UIFont.boldSystemFont(ofSize: 17)
-        ]
-        let selectedAttributes: [NSAttributedString.Key: Any] = [
-            .foregroundColor: UIColor.white,
-            .font: UIFont.boldSystemFont(ofSize: 17)
-            
-        ]
-        $0.setTitleTextAttributes(normalAttributes, for: .normal)
-        $0.setTitleTextAttributes(selectedAttributes, for: .selected)
-        $0.tintColor = .clear
-    }
-    
-    let segmentDivider = UIView().then {
-        $0.backgroundColor = .white
-    }
-    
-    let divider = UIView().then {
-        $0.backgroundColor = .white
     }
     
     let dayLabel = UILabel().then {
         $0.text = "August 8 . Friday"
         $0.font = .systemFont(ofSize: 17, weight: .bold)
-        $0.textColor = .white
+        $0.textColor = .black
     }
     
-    let locationLabel = UILabel().then {
-        $0.text = "Seoul, Korea"
-        $0.font = .systemFont(ofSize: 17, weight: .bold)
-        $0.textColor = .white
+    let locationCityLabel = UILabel().then {
+        $0.text = "Seoul"
+        $0.font = .systemFont(ofSize: 32, weight: .bold)
+        $0.textColor = .black
+    }
+    
+    let locationCountryLabel = UILabel().then {
+        $0.text = "Korea"
+        $0.font = .systemFont(ofSize: 15, weight: .semibold)
+        $0.textColor = .black
     }
     
     let locationImageView = UIImageView().then {
         $0.image = UIImage(systemName: "location.circle.fill")
-        $0.tintColor = .white
+        $0.tintColor = .black
+        $0.contentMode = .scaleAspectFit
     }
     
     let locationStackView = UIStackView().then {
@@ -73,87 +47,102 @@ class DustyViewController: UIViewController{
         $0.spacing = 8
     }
     
+    let optionSegment = CustomSegmentedControl(items: ["AQI","PM10","PM2.5","Q3","NO2","CO","SO2"]).then{
+        $0.setDividerImage(UIImage(), forLeftSegmentState: .normal, rightSegmentState: .normal, barMetrics: .default)
+        $0.setTitleTextAttributes([.foregroundColor: UIColor.gray], for: .normal)
+        $0.setTitleTextAttributes([.foregroundColor: UIColor.black], for: .selected)
+        $0.selectedSegmentTintColor = UIColor(red: 176/255, green: 216/255, blue: 175/255, alpha: 1)
+    }
+    
+    var longitude: Double = 0
+    
+    var latitude: Double = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor(named: "sunBackground")
+        self.backgroundColor = UIColor(named: "dustFineColor")
         
-        setupSegmentedControlControl()
         setupLayout()
+        startDotAnimation()
+        LocationManager.shared.requestLocation { location in
+            guard let location = location else { return }
+            self.latitude = location.coordinate.latitude
+            self.longitude = location.coordinate.longitude
+            DispatchQueue.main.async {
+                self.fetchDustyData()
+            }
+            print("Latitude: \(self.latitude)")
+            print("Longitude: \(self.longitude)")
+        }
     }
     
-    func setupSegmentedControlControl(){
-        segmentedControl.addTarget(self, action: #selector(didChangeSegmentControl(_:)), for: .valueChanged)
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        startDotAnimation()
     }
     
-    @objc func didChangeSegmentControl(_ sender: UISegmentedControl){
-        switch sender.selectedSegmentIndex {
-        case 0:
-            dotAnimationView.backgroundColor = .gray
-        case 1:
-            dotAnimationView.backgroundColor = .yellow
-        default:
-            break
-        }
-        
-        let selectedSegmentWidth = sender.bounds.width / CGFloat(sender.numberOfSegments)
-        let selectedSegmentOriginX = selectedSegmentWidth * CGFloat(sender.selectedSegmentIndex)
-        
-        segmentDivider.snp.remakeConstraints {
-            $0.top.equalTo(segmentedControl.snp.bottom)
-            $0.height.equalTo(2)
-            $0.width.equalTo(selectedSegmentWidth) // 선택된 세그먼트의 너비로 설정
-            $0.leading.equalTo(segmentedControl.snp.leading).offset(selectedSegmentOriginX) // 선택된 세그먼트의 leading에 맞춤
-        }
-        
-        UIView.animate(withDuration: 0.3) {
-            self.view.layoutIfNeeded() // 제약 조건 변경 후 레이아웃 업데이트
-        }
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        stopDotAnimation()
+    }
+    
+    private func startDotAnimation() {
+        dotAnimationView.startAnimatingDots()
+    }
+    
+    private func stopDotAnimation() {
+        dotAnimationView.stopAnimatingDots()
     }
     
     func setupLayout() {
-        view.addSubview(segmentedControl)
-        view.addSubview(segmentDivider)
         view.addSubview(dotAnimationView)
-        view.addSubview(divider)
         view.addSubview(dayLabel)
         view.addSubview(locationStackView)
         locationStackView.addArrangedSubview(locationImageView)
-        locationStackView.addArrangedSubview(locationLabel)
-        
-        segmentedControl.snp.makeConstraints{
-            $0.top.equalTo(view.safeAreaLayoutGuide).offset(20)
-            $0.leading.trailing.equalToSuperview().inset(20)
-            $0.height.equalTo(50)
-        }
-        
-        segmentDivider.snp.makeConstraints {
-            $0.top.equalTo(segmentedControl.snp.bottom)
-            $0.leading.equalTo(segmentedControl)
-            $0.width.equalTo(segmentedControl).dividedBy(2)
-            $0.height.equalTo(2)
-        }
-        
-        dotAnimationView.snp.makeConstraints{
-            $0.top.equalTo(segmentDivider.snp.bottom).offset(105)
-            $0.centerX.equalToSuperview()
-            $0.width.height.equalTo(260)
-        }
-        
-        divider.snp.makeConstraints{
-            $0.top.equalTo(dotAnimationView.snp.bottom).offset(114)
-            $0.leading.equalToSuperview()
-            $0.trailing.equalToSuperview().inset(20)
-            $0.height.equalTo(3)
-        }
+        locationStackView.addArrangedSubview(locationCityLabel)
+        locationStackView.addArrangedSubview(locationCountryLabel)
+        view.addSubview(optionSegment)
         
         dayLabel.snp.makeConstraints{
-            $0.top.equalTo(divider.snp.bottom).offset(30)
+            $0.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(32)
             $0.leading.equalToSuperview().offset(20)
         }
         
         locationStackView.snp.makeConstraints{
-            $0.top.equalTo(dayLabel.snp.bottom).offset(10)
+            $0.top.equalTo(dayLabel.snp.bottom).offset(4)
             $0.leading.equalTo(dayLabel)
+        }
+        
+        dotAnimationView.snp.makeConstraints{
+            $0.top.equalTo(locationStackView.snp.bottom).offset(40)
+            $0.centerX.equalToSuperview()
+            $0.width.height.equalTo(280)
+        }
+        
+        optionSegment.snp.makeConstraints{
+            $0.top.equalTo(dotAnimationView.snp.bottom).offset(32)
+            $0.leading.equalToSuperview().offset(20)
+            $0.trailing.equalToSuperview().offset(-20)
+            $0.height.equalTo(40)
+        }
+    }
+    
+    func fetchDustyData() {
+        DustAPIManager.shared.getDustData(latitude: self.latitude, longitude: self.longitude) { result in
+            switch result{
+            case .success(let data):
+                print("getDustData Success: \(data.data.city.name)")
+                let city = data.data.city.name.components(separatedBy: ", ")
+                DispatchQueue.main.async{
+                    self.dayLabel.text = data.data.time.s.components(separatedBy: " ")[0]
+                    self.locationCityLabel.text = city[1]
+                    self.locationCountryLabel.text = city[2]
+                    self.dotAnimationView.aqiLabel.text = String(data.data.aqi)
+                }
+            case .failure(let error):
+                print("getDustData Failure \(error)")
+            }
         }
     }
     
