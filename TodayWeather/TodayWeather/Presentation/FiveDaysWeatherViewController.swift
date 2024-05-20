63f2c44d4da89089d6846494f8e1c055983cdb61
suri0000 <yesle2005@naver.com>
2024-05-20 23:26:35 +0900

    [Add] #12 - FiveDaysWeatherViewController

---
+++ TodayWeather/TodayWeather/Presentation/FiveDaysWeatherViewController.swift
@@ -0,0 +1,17 @@
+//
+//  FiveDaysWeatherViewController.swift
+//  TodayWeather
+//
+//  Created by 예슬 on 5/20/24.
+//
+
+import UIKit
+
+class FiveDaysWeatherViewController: UIViewController {
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+}
