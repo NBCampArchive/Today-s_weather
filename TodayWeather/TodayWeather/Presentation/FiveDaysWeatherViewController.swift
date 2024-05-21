8d6d796e179c83c189541566eda19814bb3e3ac9
suri0000 <yesle2005@naver.com>
2024-05-21 10:47:42 +0900

    [Feat] #12 - 주간 날씨 UI

--- TodayWeather/TodayWeather/Presentation/FiveDaysWeatherViewController.swift
+++ TodayWeather/TodayWeather/Presentation/FiveDaysWeatherViewController.swift
@@ -9,9 +9,49 @@ import UIKit
 
 class FiveDaysWeatherViewController: UIViewController {
     
-
+    private let tableView = UITableView(frame: .zero, style: .grouped).then {
+        $0.separatorStyle = .none
+        $0.backgroundColor = .rainyBackground
+    }
+    // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .blue
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.register(WeatherTableViewHeaderView.self, forHeaderFooterViewReuseIdentifier: WeatherTableViewHeaderView.identifier)
+        tableView.register(FiveDaysWeatherTableViewCell.self, forCellReuseIdentifier: FiveDaysWeatherTableViewCell.identifier)
+        
+        configureUI()
+        setConstraints()
+    }
+    // MARK: - UI 함수
+    private func configureUI() {
+        view.backgroundColor = .rainyBackground
+        view.addSubview(tableView)
+    }
+    
+    private func setConstraints() {
+        tableView.snp.makeConstraints {
+            $0.edges.equalTo(self.view.safeAreaLayoutGuide)
+        }
+    }
+}
+// MARK: - UITableViewDelegate, UITableViewDataSource
+
+extension FiveDaysWeatherViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        guard let headerView = tableView.dequeueReusableHeaderFooterView(withIdentifier: WeatherTableViewHeaderView.identifier) as? WeatherTableViewHeaderView else { return UIView() }
+        
+        return headerView
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: FiveDaysWeatherTableViewCell.identifier, for: indexPath) as? FiveDaysWeatherTableViewCell else { return UITableViewCell() }
+        return cell
     }
 }
